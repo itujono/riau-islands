93ceca6a88cad57475e047ef5a74c978ecea7c3c
itujono <itujono@gmail.com>
2019-01-16 17:17:42 +0700

    Layout logic interpages.

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
 import './layout.css'
 
-const Layout = ({ children }) => (
+const Layout = ({ children, pathname }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -22,7 +22,7 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: '0 auto',
-            maxWidth: 960,
+            maxWidth: pathname && pathname.includes('/post/') ? undefined : 960,
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
@@ -36,6 +36,7 @@ const Layout = ({ children }) => (
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  pathname: PropTypes.string
 }
 
 export default Layout
--- src/templates/article.js
+++ src/templates/article.js
@@ -1,74 +1,85 @@
 import React, { Component } from 'react'
 import { graphql, Link } from "gatsby";
-import { Row, Col, List, Button, Icon } from 'antd';
+import Layout from "../components/layout";
+import { Row, Col, List, Button, Icon, Divider } from 'antd';
 
 
 
 class Article extends Component {
     render() {
-        const { title, image, content, createdAt, author } = this.props.data.contentfulArticle
+        const { title, image, content, createdAt, author, tags } = this.props.data.contentfulArticle
         const { next, prev } = this.props.pageContext
+        const pathname = this.props.location.pathname
 
         return (
-            <div className="article-page">
-                <Row type="flex" justify="center" align="middle">
-                    <Col span={12}>
-                        <Row className="navigator">
-                            <Col>
-                                <Link to="../">
-                                    <Button type="dashed">
-                                        <Icon type="left" />
-                                        Back
-                                    </Button>
-                                </Link>
-                            </Col>
-                        </Row>
-                        <Row className="title">
-                            <Col>
-                                <h2>{title}</h2>
-                                <List grid={{ gutter: 16, column: 4 }}>
-                                    <List.Item>
-                                        { createdAt }
-                                    </List.Item>
-                                    <List.Item>
-                                        { author }
-                                    </List.Item>
-                                </List>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <img src={image.file.url} alt={image.file.fileName} />
-                            </Col>
-                        </Row>
-                        <Row className="content">
-                            <Col>
-                                { content.content }
-                            </Col>
-                        </Row>
-                        <Row type="flex" justify="space-between" className="prev-next">
-                            <Col>
-                                { prev && <div className="prev">
-                                    <strong>Sebelumnya</strong> <br />
-                                    <Link to={'post/' + prev.node.slug}>
-                                        <Icon type="left" /> &nbsp;
-                                        { prev && prev.node.title }
+            <Layout pathname={pathname}>
+                <div className="article-page">
+                    <Row type="flex" justify="center" align="middle">
+                        <Col span={12}>
+                            <Row className="navigator">
+                                <Col>
+                                    <Link to="../">
+                                        <Button type="dashed">
+                                            <Icon type="left" />
+                                            Back
+                                        </Button>
                                     </Link>
-                                </div> }
-                            </Col>
-                            <Col>
-                                { next && <div className="next">
-                                    <strong>Selanjutnya</strong> <br />
-                                    <Link to={'post/' + next.node.slug}>
-                                        { next && next.node.title } &nbsp;
-                                        <Icon type="right" />
-                                    </Link>
-                                </div> }
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </div>
+                                </Col>
+                            </Row>
+                            <Row className="title">
+                                <Col>
+                                    <h2>{title}</h2>
+                                    <List grid={{ gutter: 16, column: 4 }}>
+                                        <List.Item>
+                                            { createdAt }
+                                        </List.Item>
+                                        <List.Item>
+                                            { author }
+                                        </List.Item>
+                                    </List>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <img src={image.file.url} alt={image.file.fileName} />
+                                </Col>
+                            </Row>
+                            <Row className="content">
+                                <Col>
+                                    { content.content }
+                                </Col>
+                                <Divider dashed />
+                                <Row className="tags">
+                                    <Col>
+                                        Ada di tag <br />
+                                        { tags.map(tag => <Link to={`tag/${tag}`}>{`#${tag}`} &nbsp; </Link>) }
+                                    </Col>
+                                </Row>
+                            </Row>
+                            <Row type="flex" justify="space-between" className="prev-next">
+                                <Col>
+                                    { prev && <div className="prev">
+                                        <strong>Sebelumnya</strong> <br />
+                                        <Link to={'post/' + prev.node.slug}>
+                                            <Icon type="left" /> &nbsp;
+                                            { prev && prev.node.title }
+                                        </Link>
+                                    </div> }
+                                </Col>
+                                <Col>
+                                    { next && <div className="next">
+                                        <strong>Selanjutnya</strong> <br />
+                                        <Link to={'post/' + next.node.slug}>
+                                            { next && next.node.title } &nbsp;
+                                            <Icon type="right" />
+                                        </Link>
+                                    </div> }
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </div>
+            </Layout>
         )
     }
 }
@@ -82,6 +93,7 @@ export const pageQuery = graphql`
             slug
             title
             author
+            tags
             image {
                 id
                 file {
